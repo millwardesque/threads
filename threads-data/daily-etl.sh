f986ddc3e396a0d31fd9a954b9778ff2543993db
christopher.millward <christopher.millward@cloudkitchens.com>
2021-10-20 09:54:34 -0400

    Relocated queries out of this repo

--- threads-data/daily-etl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-WORKING_DIR=/Users/christopher.millward/Development/threads/threads-data/
-YESTERDAY=$(date -v -1d "+%Y-%m-%d")
-
-echo "Processing Virtual Brands orders"
-$WORKING_DIR/presto-etl.sh $WORKING_DIR/queries/virtualbrands-orders.sql $YESTERDAY 1 $WORKING_DIR/data/virtualbrands-orders.now.csv 1 1
